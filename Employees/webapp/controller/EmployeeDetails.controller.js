9765919ccadaf1713f2e6870a1d1108f81ae6690
josemchm <josemchm@gmail.com>
2021-07-01 07:17:57 +0000

    1er Commit 6.4

---
+++ Employees/webapp/controller/EmployeeDetails.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+function(Controller){
+
+    return Controller.extend("logaligroup.Employee.controller.EmployeeDetails",{
+        onInit: function(){
+
+        }
+    });
+});
