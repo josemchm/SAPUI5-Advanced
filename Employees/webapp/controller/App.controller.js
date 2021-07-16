7415d92ab53a1c1efdff9026574c6b02c484102a
josemchm <josemchm@gmail.com>
2021-07-16 04:10:34 +0000

    Laboratorio Navegacion

---
+++ Employees/webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+//@ts-nocheck
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+
+        return Controller.extend("logaligroup.Employees.controller.App", {
+
+            onInit: function () {
+            }
+
+        });
+    });
